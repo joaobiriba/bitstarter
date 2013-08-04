7055f0312662ed118215593debbdf8be049404d0
joaobiriba <glaquidara@gmail.com>
2013-08-04 19:09:36 +0000

    web.js like lecture7

--- web.js
+++ web.js
@@ -1,13 +1,12 @@
-var fs = require('fs');
-
 var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
 
-var app = express();
+var app =  express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    buf = fs.readFileSync('index.html');
-    out = buf.toString();
-    response.send(out);
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
 var port = process.env.PORT || 8080
